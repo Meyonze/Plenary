e2fc09ef7d6504a64df65cd7a81a69313bd12d97
Meyonze <lieaflet@gmail.com>
2026-06-19 19:38:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'plenary-v1';
+const ASSETS = ['./', './index.html', './manifest.json',
+                './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // 広告ドメインはキャッシュしない
+  const url = event.request.url;
+  if (url.includes('googlesyndication.com') || url.includes('doubleclick.net')) {
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
